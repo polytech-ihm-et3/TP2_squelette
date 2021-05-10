7f0693cdc08629c0fb9d9fc463486db873639a7c
EugenieBrasier <eugenie.brasier@gmail.com>
2021-05-10 21:15:09 +0200

    m

--- src/application/FileSelector.java
+++ src/application/FileSelector.java
@@ -1,88 +1,104 @@
 package application;
 
-import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
-
 import java.io.File;
 import java.util.ArrayList;
 
 public class FileSelector extends DialogPane 
 {
-    private String directory = null ;
-    private String file = null ;
-    private ArrayList<String> listFile;
-    private ArrayList<String> listRepParent;
-
-    public FileSelector() 
-    {
-        super();
-        listRepParent = new ArrayList<>();
-        listFile = new ArrayList<>();
-    }
-
-    // Retourne une Arraylist des parents du fichier Path
-    public ArrayList<String> getListParentRepositories(String path)
-    {
-        String actualPath = "";
-        File dir = new File(path) ;
-        
-        if (!dir.exists() || !dir.isDirectory()) return null ;
-
-        directory = dir.getAbsolutePath() ;
-        file = null ;
-
-        /*String[] files = dir.list() ;
-        if (files!=null) 
-        {
-            for(int i=0; i<files.length; i++) 
-            {
-                File f = new File(path, files[i]);
-                if (f.isDirectory()) files[i] = files[i]+File.separator;
-            }
-        }*/
-
-        listRepParent.clear();
-        String[] dirs = path.split("\\"+File.separator) ;
-        for (String p : dirs) 
-        {
-            actualPath += p + File.separator;
-            listRepParent.add(actualPath);
-        }
-
-        return listRepParent;
-    }
-
-    //retourne une arraylist des fichier present dans le dossier Path
-    public ArrayList<String> getContent(String path){
-    	
-
-        File dir = new File(path) ;
-
-        if (!dir.exists() || !dir.isDirectory()) return null ;
-
-        directory = dir.getAbsolutePath() ;
-        file = null ;
-
-        String[] files = dir.list() ;
-        if (files!=null) 
-        {
-            listFile.clear();
-            for(int i=0; i<files.length; i++) 
-            {
-                File f = new File(path, files[i]);
-
-                if (f.isDirectory()) 
-                {
-                    files[i] = files[i]+File.separator ;
-                    listFile.add(f.getName()+ File.separator);
-                } 
-                else if(f.isFile())
-                {
-                    listFile.add(f.getName());
-                }
-            }
-        }
-        
-        return listFile;
-    }
+	private String currentDirectory = null ;
+	private ArrayList<String> listContent;
+	private ArrayList<String> listParentDirectories;
+	
+	public FileSelector()
+	{
+		super();
+		listParentDirectories = new ArrayList<>();
+		listContent = new ArrayList<>();
+	}
+	
+	/**
+	 * Cette méthode permet de récupérer l'ensemble des dossiers parents
+	 * d'un dossier courant.
+	 * @param path Le chemin vers le dossier courant
+	 * @return La liste des dossiers parents
+	 */
+	public ArrayList<String> getListParentDirectories(String path)
+	{
+		//On crée le dossier à partir du chemin passé en paramètre
+		File pathDirectory = new File(path);
+		
+		//S'il n'existe aucun dossier correspondant à ce chemin
+		if (!pathDirectory.exists() || !pathDirectory.isDirectory())
+		{
+			return null ;
+		}
+		
+		//Si un dossier existe, on le définit comme dossier courant 
+		currentDirectory = pathDirectory.getAbsolutePath();
+		
+		String parentDirectoryPath = "";
+		listParentDirectories.clear();
+		
+		//On divise le chemin grâce à un séparateur
+		String[] directories = path.split("\\"+File.separator); //Version Windows
+		//String[] directories = path.split(File.separator); //Version Mac/Linux
+		
+		//On récupère les dossiers parents en ajoutant les éléments du chemin au fur et à mesure
+		for (String directory : directories)
+		{
+			parentDirectoryPath += directory + File.separator;
+			listParentDirectories.add(parentDirectoryPath);
+		}
+		
+		return listParentDirectories;
+	}
+	
+	/**
+	 * Cette méthode permet de récupérer l'ensemble des éléments
+	 * présents dans un dossier courant.
+	 * @param path Le chemin vers le dossier courant
+	 * @return La liste des éléments présents dans le dossier courant
+	 */
+	public ArrayList<String> getContent(String path)
+	{
+		//On crée le dossier à partir du chemin passé en paramètre
+		File pathDirectory = new File(path);
+		
+		//S'il n'existe aucun dossier correspondant à ce chemin
+		if (!pathDirectory.exists() || !pathDirectory.isDirectory())
+		{
+			return null ;
+		}
+		
+		//Si un dossier existe, on le définit comme dossier courant 
+		currentDirectory = pathDirectory.getAbsolutePath();
+		
+		//On liste les éléments du dossier
+		String[] files = pathDirectory.list() ;
+		
+		if (files != null)
+		{
+			listContent.clear();
+			
+			//On parcourt les éléments du dossier
+			for(int i=0; i<files.length; i++)
+			{
+				File file = new File(path, files[i]);
+				
+				//Si l'élément est un dossier, on ajoute un séparateur
+				if (file.isDirectory())
+				{
+					files[i] = files[i] + File.separator;
+					listContent.add(file.getName()+ File.separator);
+				}
+				else if(file.isFile())
+				{
+					listContent.add(file.getName());
+				}
+			}
+		}
+		
+		return listContent;
+	}
 }
